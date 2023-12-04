f76c8877254c0e519001a2f88392993ff6f6762f
Samuele Papa <samuele.papa@gmail.com>
2023-12-04 16:29:52 +0100

    adding the torch classifier

--- neural_dataset/__init__.py
+++ neural_dataset/__init__.py
@@ -3,3 +3,22 @@ from neural_dataset.classification_neural_datasets import (
     ClassificationNeuralMNIST,
 )
 from neural_dataset.core import PreloadedNeFDataset
+from neural_dataset.nef_normalization import compute_mean_std_for_nef_dataset
+from neural_dataset.transform import (
+    Compose,
+    Identity,
+    ListToParameters,
+    Normalize,
+    ParametersToListMFN,
+    ParametersToListSIREN,
+    RandomFourierNetWeightPermutation,
+    RandomGaussianNoise,
+    RandomMLPWeightPermutation,
+    RandomQuantileWeightDropout,
+    RandomRotate,
+    RandomScale,
+    RandomTranslateMFN,
+    RandomTranslateSIREN,
+    ToTensor,
+    UnNormalize,
+)
